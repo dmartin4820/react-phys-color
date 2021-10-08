ec287ad8d5c356a844cae93ce7bda00981cfcdec
dmartin4820 <dom4822@yahoo.com>
2021-10-07 20:08:50 -0700

    create test component with a simple div box with changing color based on a sine function and setInterval

---
+++ test-react-app/src/test_component/test_phys_color.js
@@ -0,0 +1,41 @@
+import './test_phys_color.css'
+import {useEffect, useState} from 'react'
+
+export default function Test_phys_color() {
+  const [style, setStyle] = useState({
+    backgroundColor: 'rgb(0, 0, 127.5)'
+  })
+  const [compCounter, setCounter] = useState(0)
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setCounter(compCounter => {
+        setBg(sine(compCounter, 127.5, 0.05, 127.5))
+        return compCounter + 1
+      })
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  },[])
+
+  //time, amplitude, frequency
+  function sine(t, A, f, offset) {
+    return A * Math.sin(2*Math.PI * f * t) + offset
+  }
+
+  function setBg(value) {
+    setStyle({
+      ...style,
+      backgroundColor: `rgb(0, 0, ${value})`
+    })
+  }
+
+  return (
+    <div className="container">
+      <div className="content" style={style}> Hello </div>
+      <span>{sine(compCounter, 127.5, 0.1, 127.5)}</span>
+    </div>
+  )
+}
