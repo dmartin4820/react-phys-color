20247485fdd334f78e60447e043b2a7037fc0576
dmartin4820 <dom4822@yahoo.com>
2021-10-20 21:47:34 -0700

    add function for returning user chosen mathematical function and add function to return current value of chosen math function

--- utils/functions.js
+++ utils/functions.js
@@ -1,2 +1,15 @@
-exports.sine = (t, a, f, offset) => a * Math.sin(2 * Math.PI * f * t) + offset
-exports.exp = (t, a, b, c) => a * Math.exp(b* t) + c
+exports.sine = (t, params) => params.a * Math.sin(2 * Math.PI * params.freq * t) + params.offset
+exports.exp = (t, params) => params.a * Math.exp(params.b* t) + params.c
+exports.getFunction = (name) => {
+  switch(name) {
+    case 'sine':
+      return this.sine
+    case 'exp':
+      return this.exp
+    default:
+      return this.sine
+  }
+}
+exports.getCurrValue = (f, t, params) => {
+  return f(t, params)
+}
